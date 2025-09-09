413ad91815c4b5a0de46370a5d97d4cc1c46868a
Delta Mikhailovna <irfan2904.ia@gmail.com>
2025-09-09 17:55:49 +0700

    first up

---
+++ app/Models/ActivityList.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityList extends Model
+{
+    /** @use HasFactory<\Database\Factories\ActivityListFactory> */
+    use HasFactory;
+}
