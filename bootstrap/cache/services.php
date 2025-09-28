f0fbcb99e91307f487b72ee2d5d2359320994660
Kezia Sihombing <keziasihombing184@gmail.com>
2025-09-28 13:29:50 +0700

    Install tymon/jwt-auth package for JWT authentication

--- bootstrap/cache/services.php
+++ bootstrap/cache/services.php
@@ -30,6 +30,7 @@
     26 => 'Carbon\\Laravel\\ServiceProvider',
     27 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     28 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    29 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
   ),
   'eager' => 
   array (
@@ -47,6 +48,7 @@
     11 => 'Carbon\\Laravel\\ServiceProvider',
     12 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     13 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    14 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
   ),
   'deferred' => 
   array (
