1f34d67ee9685777efb207bcc954413939283bac
Daniel <irfan2904.ia@gmail.com>
2025-09-30 16:57:46 +0700

    delete unused api route

--- routes/api.php
+++ routes/api.php
@@ -102,45 +102,6 @@ Route::post('/following', [FollowingController::class, 'store']); //memfollow pe
 Route::patch('/following/{organizer}/notifications', [FollowingController::class, 'update']); //update notifikasi
 Route::delete('/following/{organizer}', [FollowingController::class, 'destroy']); //batal follow
 
-// Route::prefix('activity_request')->group(function () {
-//     Route::post('/', [ActivityRequestController::class, 'store']);
-//     Route::get('/', [ActivityRequestController::class, 'index']);
-//     Route::get('/mine', [ActivityRequestController::class, 'mine']);
-//     Route::get('/{id}', [ActivityRequestController::class, 'show']);
-//     Route::put('/{id}', [ActivityRequestController::class, 'update']);
-//     Route::delete('/{id}', [ActivityRequestController::class, 'destroy']);
-//     Route::patch('/{id}/approve', [ActivityRequestController::class, 'approve']);
-//     Route::patch('/{id}/reject', [ActivityRequestController::class, 'reject']);
-// });
-
-// // --- USER (umum)
-// Route::get('/users', [UserController::class, 'index']);
-// Route::get('/users/{id}', [UserController::class, 'show']);
-// Route::post('/users', [UserController::class, 'store']);
-// Route::put('/users/{id}', [UserController::class, 'update']);
-// Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-// // --- ADMIN
-// Route::get('/admins', [AdminController::class, 'index']);
-// Route::get('/admins/{id}', [AdminController::class, 'show']);
-// Route::post('/admins', [AdminController::class, 'store']);
-// Route::put('/admins/{id}', [AdminController::class, 'update']);
-// Route::delete('/admins/{id}', [AdminController::class, 'destroy']);
-
-// // --- ORGANIZER
-// Route::get('/organizers', [OrganizerController::class, 'index']);
-// Route::get('/organizers/{id}', [OrganizerController::class, 'show']);
-// Route::post('/organizers', [OrganizerController::class, 'store']);
-// Route::put('/organizers/{id}', [OrganizerController::class, 'update']);
-// Route::delete('/organizers/{id}', [OrganizerController::class, 'destroy']);
-
-// // --- VOLUNTEER
-// Route::get('/volunteers', [VolunteerController::class, 'index']);
-// Route::get('/volunteers/{id}', [VolunteerController::class, 'show']);
-// Route::post('/volunteers', [VolunteerController::class, 'store']);
-// Route::put('/volunteers/{id}', [VolunteerController::class, 'update']);
-// Route::delete('/volunteers/{id}', [VolunteerController::class, 'destroy']);
-
 // Rute untuk mendapatkan informasi user berdasarkan ID (contoh)
 Route::get('/user/{id}', [UserController::class, 'show']);
 
